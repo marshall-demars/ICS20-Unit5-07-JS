90387cc8af32b74e87f7dee587a293e4bec220b5
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-10 17:13:16 +0000

    3 files

--- js/script.js
+++ js/script.js
@@ -13,9 +13,35 @@ if (navigator.serviceWorker) {
   })
 }
 
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
 /**
- * This function displays an alert.
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit5-06-JS/sw.js", {
+    scope: "/ICS20-Unit5-06-JS/",
+  })
+}
+
+'use strict'
+
+/**
+ * This function calculates if you get free admission
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  // input
+  var firstInteger = document.getElementById("integer-one").value
+
+    // process and output
+  var total = 0;
+  
+  for (let counter = 0; counter <= firstInteger; counter ++) {
+     total += counter;
+  }
+  document.getElementById("loop").innerHTML = "The number is " + (total)
 }
